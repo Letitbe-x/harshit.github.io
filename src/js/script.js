8c2c24d92c313185e395f2125ffa469041f81636
Harshit <sharmaharshitraj@gmail.com>
2024-01-04 21:04:36 +0530

    Refined overall animation

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,40 @@
-// Get references to the menu toggle button and the mobile menu itself
+// Get references to the menu toggle button and the mobile menu
 const menuToggle = document.querySelector('.menu-toggle');
 const mobileMenu = document.querySelector('.mobile-menu');
 
-// Toggle the 'active' class to show/hide the mobile menu
+// Add a click event listener to the menu toggle button
 menuToggle.addEventListener('click', function() {
-  // Toggle the 'active' class on the menu toggle button
-  menuToggle.classList.toggle('active');
-  
-  // Check if the mobile menu is currently active (displayed)
-  const isMenuActive = mobileMenu.classList.contains('active');
-
-  // Toggle the 'active' class on the mobile menu
-  if (isMenuActive) {
-    // If the menu is active, remove the class to hide it
-    mobileMenu.classList.remove('active');
+  // Check if the mobile menu is currently active
+  if (mobileMenu.classList.contains('active')) {
+    // If active, close the mobile menu
+    closeMobileMenu();
   } else {
-    // If the menu is inactive, add the class to display it
-    mobileMenu.classList.add('active');
+    // If not active, open the mobile menu
+    openMobileMenu();
   }
 });
+
+// Function to open the mobile menu
+function openMobileMenu() {
+  // Add the 'active' class to the menu toggle button
+  menuToggle.classList.add('active');
+  // Delay adding the 'clicked' class for a smoother transition
+  setTimeout(() => {
+    menuToggle.classList.add('clicked');
+  }, 50); // Adjust the delay time as needed
+  // Add the 'active' class to the mobile menu
+  mobileMenu.classList.add('active');
+}
+
+// Function to close the mobile menu
+function closeMobileMenu() {
+  // Add the 'closing' class to initiate the closing animation
+  mobileMenu.classList.add('closing');
+  // Remove classes and reset the menu toggle after the closing animation duration
+  setTimeout(() => {
+    mobileMenu.classList.remove('active');
+    mobileMenu.classList.remove('closing');
+    menuToggle.classList.remove('active');
+    menuToggle.classList.remove('clicked');
+  }, 300); // Duration of the slideToBottom animation
+}
